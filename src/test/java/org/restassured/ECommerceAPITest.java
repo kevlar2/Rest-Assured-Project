5590863fb3b8451e0156f98b706255adf86b04fe
Kevin Orukele-MBP <korukele84@outlook.com>
2022-11-10 21:23:47 +0000

    Added Product API

--- src/test/java/org/restassured/ECommerceAPITest.java
+++ src/test/java/org/restassured/ECommerceAPITest.java
@@ -2,9 +2,12 @@ package org.restassured;
 
 import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.http.ContentType;
+import io.restassured.path.json.JsonPath;
 import io.restassured.specification.RequestSpecification;
 import org.restassured.pojo.LoginRequest;
 import org.restassured.pojo.LoginResponse;
+
+import java.io.File;
 
 import static io.restassured.RestAssured.given;
 
@@ -31,6 +34,36 @@ public class ECommerceAPITest {
 
 
         // Add Product API
+
+        RequestSpecification addProductBaseReq = new RequestSpecBuilder().setBaseUri("https://rahulshettyacademy.com")
+                .addHeader("Authorization", loginResponse.getToken())
+                .build();
+
+        RequestSpecification addProductReq = given().log().all().spec(addProductBaseReq)
+                .param("productName", "qwerty")
+                .param("productAddedBy", loginResponse.getUserId())
+                .param("productCategory", "fashion")
+                .param("productSubCategory", "shirts")
+                .param("productPrice", "11500")
+                .param("productDescription", "Addias Originals")
+                .param("productFor", "men")
+                .multiPart("productImage", new File(System.getProperty("user.dir") +
+                        "//src//main//java//org//restassured//files//adidas.png"));
+
+
+
+        String reqToAddProduct = addProductReq.when().post("/api/ecom/product/add-product").then().log().all()
+                .extract().response()
+                .asString();
+        JsonPath js = new JsonPath(reqToAddProduct);
+
+        String productId = js.getString("productId");
+
+        // Create Order API
+
+        // Get the order details API
+
+        // Delete-product API
 
     }
 
